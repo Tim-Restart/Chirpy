9fde5c1c6ab1344e57515c41dec46fca21db3caf
Phybar <tim.e.janke@gmail.com>
2025-03-21 10:46:38 +1000

    Made counter and handler functions

--- main.go
+++ main.go
@@ -1,15 +1,62 @@
 package main
 
 import "net/http"
+import "sync/atomic"
+import "fmt"
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	// Increments the fileserverHits
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Increment the counter using atomic32 add
+		cfg.fileserverHits.Add(1)
+		// pass next to ServerHTTP
+		next.ServeHTTP(w, r)
+	}
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	counter := cfg.fileserverHits.Load()
+	w.Write([]byte(fmt.Sprintf("Hits: %d", counter)))
+}
+
+func (cfg *apiConfig) metricsResetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte("Counter Reset"))
+}
 
 
 func main() {
 	// Make a new server
 	mux := http.NewServeMux()
 
+	// Assignes fileHandler so that it can be called in mux.Handle
+	fileHandler := http.FileServer(http.Dir("./"))
+
 	// Register paths and their handlers
 	// FileServer is in http package, Dir converts the '.' to a directory part
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", middlewareMetricsInc(http.StripPrefix("/app", fileHandler)))
+
+	// returns the server metrics
+	mux.HandleFunc("/metrics", apiConfig.metricsHandler)
+
+	// Resets the server metrics
+	mux.HandleFunc("/reset", apiConfig.metricsResetHandler)
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	// Set the content type header
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+	// Write the status code
+	w.WriteHeader(http.StatusOK)
+
+	// Write the response body
+	w.Write([]byte("OK"))
+
+	})
 
 	// Create a new Server struct
 	server := &http.Server{
@@ -17,7 +64,9 @@ func main() {
 		Handler: mux,
 	}
 
+
 	// Start the server
 	server.ListenAndServe()
+	
 
 }
