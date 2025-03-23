09afb0b0d1bdbb250d659244da0b8ec511c60a60
Phybar <tim_restart.proton.me>
2025-03-23 20:46:45 +1000

    handler file added

--- main.go
+++ main.go
@@ -1,50 +1,10 @@
 package main
 
 import "net/http"
-import "sync/atomic"
-import "fmt"
 import "encoding/json"
 import "log"
 
-type apiConfig struct {
-	fileserverHits atomic.Int32
-}
 
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	// Increments the fileserverHits
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Increment the counter using atomic32 add
-		cfg.fileserverHits.Add(1)
-		// pass next to ServerHTTP
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
-	counter := cfg.fileserverHits.Load()
-
-	//HTML template for admin page
-	htmlTemplate := `<html>
-	<body>
-		<h1>Welcome, Chirpy Admin</h1>
-		<p>Chirpy has been visited %d times!</p>
-	</body>
-	</html>`
-
-	// Format the above template with the counter in the %d spot
-	htmlContent := fmt.Sprintf(htmlTemplate, counter)
-
-	// Sets the header type to HTML
-	w.Header().Set("Content-Type", "text/html")
-	
-	// Write the html to the response
-	fmt.Fprint(w, htmlContent)
-}
-
-func (cfg *apiConfig) metricsResetHandler(w http.ResponseWriter, r *http.Request) {
-	cfg.fileserverHits.Store(0)
-	w.Write([]byte("Counter Reset\n"))
-}
 
 
 func main() {
