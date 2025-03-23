19bfb51cc8127a39d25dddb499e66fd175cb8f1b
Phybar <tim_restart.proton.me>
2025-03-23 20:30:56 +1000

    language filter added

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import "net/http"
 import "sync/atomic"
 import "fmt"
+import "encoding/json"
+import "log"
+import "strings"
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
@@ -44,6 +47,44 @@ func (cfg *apiConfig) metricsResetHandler(w http.ResponseWriter, r *http.Request
 	w.Write([]byte("Counter Reset\n"))
 }
 
+func badWordReplacement(chirpy string) ([]byte) {
+	//Struct for formating the JSON response
+	type cleanedBody struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+
+	// Assign params body to a variable so I can keep overwriting it (not sure if thats a good idea)
+	// not needed for this way - chirp := params.Body
+	chirp := strings.Split(chirpy, " ")
+	for i := 0; i < len(chirp); i++ {
+		if strings.ToLower(chirp[i]) == "kerfuffle" {
+			chirp[i] = "****"
+		} else if strings.ToLower(chirp[i]) == "sharbert" {
+			chirp[i] = "****"
+		} else if strings.ToLower(chirp[i]) == "fornax" {
+			chirp[i] = "****"
+		} else {
+			continue
+		}
+	}
+
+	// Joins the array back together
+	modifiedChirp := strings.Join(chirp, " ")
+
+	// Assign the cleaned chirp to the struct for JSON marshalling
+	cleanedChirp := cleanedBody {
+		CleanedBody: modifiedChirp,
+	}
+
+	// Marshal the response into JSON and check it works
+	jsonResp, err := json.Marshal(cleanedChirp)
+			if err != nil {
+				log.Printf("Error marshalling JSON %s", err)
+				return nil
+			}
+	return jsonResp
+}
+
 
 func main() {
 
@@ -59,6 +100,95 @@ func main() {
 	// Register paths and their handlers
 	// FileServer is in http package, Dir converts the '.' to a directory part
 	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", fileHandler)))
+
+	// vaidates the 140 characters of the chirp
+	mux.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
+		
+		// Struct for incoming JSON posts
+		type parameters struct {
+			Body string `json:"body"`
+		}
+		// Successs repsonse struct
+		type successResponse struct {
+			Valid bool `json:"valid"`
+		}
+		// Error response struct
+		type errorResponse struct {
+			Error string `json:"error"`
+		}
+
+		decoder := json.NewDecoder(r.Body)
+		params := parameters{}
+		err := decoder.Decode(&params)
+		if err != nil {
+			errResp := errorResponse{
+				Error: "Something went wrong",
+			}
+
+			jsonResp, err := json.Marshal(errResp)
+			if err != nil {
+				log.Printf("Error marshalling JSON %s", err)
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(500)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(500)
+			w.Write(jsonResp)
+			return
+			
+		}
+
+		// Checks the length of the chirp
+		if len(params.Body) > 140 {
+			errResp := errorResponse{
+				Error: "Chirp is too long",
+			}
+
+			jsonResp, err := json.Marshal(errResp)
+			if err != nil {
+				log.Printf("Error marshalling JSON %s", err)
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(400)
+				w.Write(jsonResp)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(400)
+			w.Write(jsonResp)
+			return
+		}
+
+		type cleanedBody struct {
+			Cleaned string `json:"cleaned"`
+		}
+
+		chirp := badWordReplacement(params.Body)
+		
+		
+
+		//successChirp := successResponse{
+		//	Valid: true,
+		//}
+
+		//jsonResp, err := json.Marshal(successChirp)
+		//if err != nil {
+		//	log.Printf("Error marshalling JSON %s", err)
+		//	w.Header().Set("Content-Type", "application/json")
+		//	w.WriteHeader(500)
+		//	return
+		//}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		w.Write(chirp)
+		
+
+	})
+
+	// mux.HandleFunc()
 
 	// returns the server metrics
 	mux.HandleFunc("GET /admin/metrics", cfg.metricsHandler)
